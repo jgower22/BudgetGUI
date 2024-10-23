14bf854003fa89e5a9e10794c0caf96bb7989db6
jackson.gower@gmail.com <jackson.gower@gmail.com>
2024-10-23 13:05:55 -0400

    Add light/dark mode functionality with user preference saved in properties

--- src/budgetgui/BudgetGUI.java
+++ src/budgetgui/BudgetGUI.java
@@ -7,18 +7,35 @@ package budgetgui;
 
 import com.formdev.flatlaf.FlatLightLaf;
 import com.formdev.flatlaf.FlatDarkLaf;
+import java.util.prefs.Preferences;
+
 /**
  *
  * @author jacksongower
  */
 public class BudgetGUI {
 
+    private boolean isDarkMode = false;
+    private static final String DARK_MODE_PREF_KEY = "darkModeEnabled";
+    private Preferences prefs;
+
     /**
      * @param args the command line arguments
      */
+    public BudgetGUI() {
+        prefs = Preferences.userNodeForPackage(SettingsFrame.class);
+        isDarkMode = prefs.getBoolean(DARK_MODE_PREF_KEY, false);
+    }
+
     public static void main(String[] args) {
-        FlatLightLaf.setup();
+        BudgetGUI budgetGUI = new BudgetGUI();
+        SettingsFrame settings = new SettingsFrame(false);
+        if (budgetGUI.isDarkMode) {
+            settings.displayDarkMode();
+        } else {
+            settings.displayLightMode();
+        }
         YearFrame yearFrame = new YearFrame();
     }
-    
+
 }
